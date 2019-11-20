1e36dc61d33e58e19e5f54943de026a83e47fadd
chlwodud77 <uniform3000@naver.com>
2019-11-20 23:03:44 +0900

    update navbar function

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,7 +2,10 @@
 // If you want to make other Component, Copy and Refactor this Component.
 
 import React, { Component } from "react";
+import cx from "classnames";
 import logo from "../../logo.svg";
+import "bootstrap/dist/css/bootstrap.css";
+
 import { 
   Navbar,
   NavItem,
@@ -10,53 +13,28 @@ import {
   NavbarBrand,
   Nav
 } from "reactstrap";
-import cx from "classnames";
-import DashboardOutlinedIcon from '@material-ui/icons/DashboardOutlined';
-import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
-import NotificationsNoneOutlinedIcon from '@material-ui/icons/NotificationsNoneOutlined';
-import useScrollTrigger from '@material-ui/core/useScrollTrigger';
-import PropTypes from 'prop-types';
-import Slide from '@material-ui/core/Slide';
-import AppBar from '@material-ui/core/AppBar';
-import "bootstrap/dist/css/bootstrap.css";
+import {
+  DashboardOutlined,
+  AccountCircleOutlined,
+  NotificationsNoneOutlined
+} from "@material-ui/icons";
+import {
+  AppBar,
+  Slide,
+  useScrollTrigger
+} from "@material-ui/core";
 
 
 const defaultProps = {};
 const propTypes = {};
 
-function HideOnScroll(props) {
-  const { isActive } = props;
+const HideOnScroll = (props) => {
+  const { children } = props;
   const trigger = useScrollTrigger({disableHysteresis: true, threshold: 100});
 
   return (
     <Slide appear={false} direction="down" in={!trigger}>
-      <AppBar>
-      <Navbar className={cx(null, {
-          "navBar__borderline-bottom": isActive !== "NotLoggedIn"
-        })}> 
-          <NavbarBrand className="navBar__logo " fixed="top">
-            <NavLink className="navBar__logo__img" href="/"><img src={logo}  width="35px" alt="logo" /></NavLink>
-            { isActive === "SignUp" || isActive === "LoggedIn" ? (
-              <p className="navBar__logo__text">Bletcher</p>
-            ) : null}
-          </NavbarBrand>
-          <Nav className="navBar__items" >
-            { isActive === "NotLoggedIn" ? (
-              <NavItem className="navBar__items__item">
-                <NavLink href ="/signup">Sign Up</NavLink>
-                <NavLink href="/about">About</NavLink>
-              </NavItem>
-            ) : null}
-            { isActive === "LoggedIn" ? (
-              <NavItem className="navBar__items__item">
-                <NavLink href =""><DashboardOutlinedIcon alt="feed"/></NavLink>
-                <NavLink href=""><AccountCircleOutlinedIcon alt="My page"/></NavLink>
-                <NavLink href=""><NotificationsNoneOutlinedIcon alt="Notification"/></NavLink>
-              </NavItem>
-            ) : null}
-          </Nav>
-        </Navbar>
-        </AppBar>
+      {children}
     </Slide>
   );
 }
@@ -68,9 +46,37 @@ class NavBar extends Component {
   }
 
   render() {
-    const props = this.props;
+    const { isActive } = this.props;
     return (
-      <HideOnScroll {...props}/>
+      <HideOnScroll>
+        <AppBar>
+          <Navbar className={cx(null, {
+          "navBar__borderline-bottom": isActive !== "NotLoggedIn"
+           })}> 
+            <NavbarBrand className="navBar__logo " fixed="top">
+              <NavLink className="navBar__logo__img" href="/"><img src={logo}  width="35px" alt="logo" /></NavLink>
+              { isActive === "SignUp" || isActive === "LoggedIn" ? (
+                <p className="navBar__logo__text">Bletcher</p>
+              ) : null}
+            </NavbarBrand>
+            <Nav className="navBar__items" >
+              { isActive === "NotLoggedIn" ? (
+                <NavItem className="navBar__items__item">
+                  <NavLink href ="/signup">Sign Up</NavLink>
+                  <NavLink href="/about">About</NavLink>
+                </NavItem>
+              ) : null}
+              { isActive === "LoggedIn" ? (
+                <NavItem className="navBar__items__item">
+                  <NavLink href =""><DashboardOutlined alt="feed"/></NavLink>
+                  <NavLink href=""><AccountCircleOutlined alt="My page"/></NavLink>
+                  <NavLink href=""><NotificationsNoneOutlined alt="Notification"/></NavLink>
+                </NavItem>
+              ) : null}
+            </Nav>
+        </Navbar>
+        </AppBar>
+      </HideOnScroll>
     );
       
     
