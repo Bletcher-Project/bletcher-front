b79c3171b282b76a31611fa54916c4f16aa067a7
chlwodud77 <uniform3000@naver.com>
2019-11-25 21:15:28 +0900

    add signupPage user type choice page

--- src/Pages/SignUpPage/SignUpPage.js
+++ src/Pages/SignUpPage/SignUpPage.js
@@ -1,6 +1,9 @@
 import React, { Component } from "react";
 import { NavBar } from "../../Components";
 
+import logo_sketcher from "../../Assets/images/logo_sketcher.png";
+import logo_creator from "../../Assets/images/logo_creator.png";
+
 const defaultProps = {};
 const propTypes = {};
 
@@ -12,8 +15,31 @@ class SignUpPage extends Component {
 
   render() {
     return (
-      <div>
+      <div className="signupPage">
         <NavBar isActive="SignUp" />
+        <div className="signupPage__type">
+          <p className="signupPage__type-head">
+            Choose your type.
+          </p>
+          <div className="signupPage__type__btn">
+            <img className="signupPage__type__btn-img"
+              src={logo_sketcher}
+              width="200px" />
+            <p className="signupPage__type__btn-head">Sketcher</p>
+            <p className="signupPage__type__btn-describe">
+              Recreate with your creative idea.
+            </p>
+          </div>
+          <div className="signupPage__type__btn">
+            <img className="signupPage__type__btn-img"
+              src={logo_creator}
+              width="200px" />
+            <p className="signupPage__type__btn-head">Creator</p>
+            <p className="signupPage__type__btn-describe">
+              Share your creation for sketchers.
+            </p>
+          </div>
+        </div>_
       </div>
     );
   }
