a4d5234a3ffd6cadb64ee2a59efff9b8e0666aaf
chlwodud77 <uniform3000@naver.com>
2019-11-26 23:59:10 +0900

    choice page with material ui #2

--- src/Pages/SignUpPage/SignUpPage.js
+++ src/Pages/SignUpPage/SignUpPage.js
@@ -1,20 +1,26 @@
 import React, { Component } from "react";
 import { NavBar } from "../../Components";
 
-import { styled } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
 import logo_sketcher from "../../Assets/images/logo_sketcher.png";
 import logo_creator from "../../Assets/images/logo_creator.png";
+import { withStyles } from "@material-ui/core/styles";
+import { Box } from "@material-ui/core";
+import { purple } from "@material-ui/core/colors";
 
 const defaultProps = {};
 const propTypes = {};
 
-const TypeButton = styled(Button)({
-  border: 2,
-  borderRadius: 5,
-  borderColor: "grey.500",
-});
+const TypeButton = withStyles(theme => ({
+  root: {
+    color: theme.palette.getContrastText(purple[500]),
+    backgroundColor: purple[500],
+    '&:hover': {
+      backgroundColor: purple[700],
+    },
+  },
+}))(Button);
 
 class SignUpPage extends Component {
   constructor(props) {
@@ -31,8 +37,8 @@ class SignUpPage extends Component {
             Choose your user type.
           </p>
           <div className="signupPage__type__btn">
-            <TypeButton>
-              <img className="signupPage__type__btn-img" src={logo_sketcher} />
+            <TypeButton variant="root">
+              <img className="signupPage__type__btn-img" src={logo_sketcher} alt="sketcher"/>
               <p className="signupPage__type__btn-head">Sketcher</p>
               <p className="signupPage__type__btn-describe">
                 Recreate with your creative idea.
@@ -40,8 +46,8 @@ class SignUpPage extends Component {
             </TypeButton>
           </div>
           <div className="signupPage__type__btn">
-            <TypeButton variant="outlined">
-              <img className="signupPage__type__btn-img" src={logo_creator} />
+            <TypeButton>
+              <img className="signupPage__type__btn-img" src={logo_creator} alt="creator"/>
               <p className="signupPage__type__btn-head">Creator</p>
               <p className="signupPage__type__btn-describe">
                 Share your creation for Sketchers.
