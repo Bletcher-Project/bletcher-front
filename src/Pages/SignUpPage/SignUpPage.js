6a5066af97a52f80a01b4b458d1df62fef5dd3c2
chlwodud77 <uniform3000@naver.com>
2019-11-27 10:41:23 +0900

    choice page with material ui #3

--- src/Pages/SignUpPage/SignUpPage.js
+++ src/Pages/SignUpPage/SignUpPage.js
@@ -1,33 +1,20 @@
 import React, { Component } from "react";
-import { NavBar } from "../../Components";
-
-import Button from '@material-ui/core/Button';
+import { NavBar, TypeButton } from "../../Components";
 
 import logo_sketcher from "../../Assets/images/logo_sketcher.png";
 import logo_creator from "../../Assets/images/logo_creator.png";
-import { withStyles } from "@material-ui/core/styles";
-import { Box } from "@material-ui/core";
-import { purple } from "@material-ui/core/colors";
 
 const defaultProps = {};
 const propTypes = {};
 
-const TypeButton = withStyles(theme => ({
-  root: {
-    color: theme.palette.getContrastText(purple[500]),
-    backgroundColor: purple[500],
-    '&:hover': {
-      backgroundColor: purple[700],
-    },
-  },
-}))(Button);
+
 
 class SignUpPage extends Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
-
+  
   render() {
     return (
       <div className="signupPage">
@@ -37,7 +24,7 @@ class SignUpPage extends Component {
             Choose your user type.
           </p>
           <div className="signupPage__type__btn">
-            <TypeButton variant="root">
+            <TypeButton>
               <img className="signupPage__type__btn-img" src={logo_sketcher} alt="sketcher"/>
               <p className="signupPage__type__btn-head">Sketcher</p>
               <p className="signupPage__type__btn-describe">
