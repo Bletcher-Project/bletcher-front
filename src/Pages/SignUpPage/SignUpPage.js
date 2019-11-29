03e5efd9e3e5226d9ea6949963e576bcbbce0320
chlwodud77 <uniform3000@naver.com>
2019-11-29 17:02:52 +0900

    Add singup personal info slide #1
    add signup personal info slide with slide animation (undesigned)

--- src/Pages/SignUpPage/SignUpPage.js
+++ src/Pages/SignUpPage/SignUpPage.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import { NavBar, TypeButton } from "../../Components";
+import { NavBar, TypeButton, MainButton } from "../../Components";
+
+import Slide from '@material-ui/core/Slide';
+import ClickAwayListener from '@material-ui/core/ClickAwayListener';
+import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 
 import logo_sketcher from "../../Assets/images/logo_sketcher.png";
 import logo_creator from "../../Assets/images/logo_creator.png";
@@ -12,8 +16,12 @@ const propTypes = {};
 class SignUpPage extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      infoChecked: false
+    };
   }
+
+
 
   render() {
     return (
@@ -24,19 +32,51 @@ class SignUpPage extends Component {
             Choose your user type.
           </p>
           <div className="signupPage__type-btn">
-            <TypeButton 
-            title="Sketcher" 
-            content="Recreate with your creative idea." 
-            logo={logo_sketcher} />
-            <TypeButton 
-            title="Creator" 
-            content="Share your creation for sketchers." 
-            logo={logo_creator} />
+            <TypeButton
+              title="Sketcher"
+              content="Recreate with your creativ e idea."
+              logo={logo_sketcher}
+              onClick={this.handleInfo}
+            />
+            <TypeButton
+              title="Creator"
+              content="Share your creation for sketchers."
+              logo={logo_creator}
+              onClick={this.handleInfo}
+            />
           </div>
         </div>
+
+        <Slide
+          direction="right"
+          in={this.state.infoChecked}
+          mountOnEnter
+          timeout={ {appear: 1000, enter: 750 , exit: 750} }
+        >
+          <div className="signupPage__info">
+            <p className="signupPage__info-head">Enter your personal information.</p>
+
+            {/* example code - undesigned */}
+            <button><ArrowBackIcon onClick={this.handleInfo}/></button>
+            <input name="email" placeholder="email"></input>
+            <input name="pwd" placeholder="password"></input>
+            <input name="pwd_confirm" placeholder="password confirm"></input>
+            <MainButton text="Next" />
+            {/* example code - undesigned */}
+
+          </div>
+        </Slide>
+
       </div>
     );
   }
+
+  handleInfo = () => {
+    console.log("button clicked!");
+    this.setState({
+      infoChecked: !this.state.infoChecked
+    });
+  };
 }
 
 SignUpPage.defaultProps = defaultProps;
