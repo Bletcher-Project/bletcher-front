ab571a03b463aa29579af568b70940bda88ca94d
chlwodud77 <uniform3000@naver.com>
2019-12-17 17:48:40 +0900

    Add profile upload button & enter key action

--- src/Pages/SignUpPage/SignUpPage.js
+++ src/Pages/SignUpPage/SignUpPage.js
@@ -6,6 +6,8 @@ import ArrowBackIcon from "@material-ui/icons/ArrowBack";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 
+import ImageUploader from "react-images-upload";
+
 import logo_sketcher from "../../Assets/images/logo_sketcher.png";
 import logo_creator from "../../Assets/images/logo_creator.png";
 
@@ -24,11 +26,11 @@ class SignUpPage extends Component {
       usertype: "",
       infoOpen: false,
       profileOpen: false,
-      isEmailValid: false,
-      isPwdValid: false,
-      isRepwdValid: false,
+      isEmailValid: true,
+      isPwdValid: true,
+      isRepwdValid: true,
       isNameValid: true,
-      isStatusValid: false
+      isStatusValid: true
     };
   }
 
@@ -133,11 +135,19 @@ class SignUpPage extends Component {
                 label="Re-password"
                 type="password"
                 onChange={this.handleRepwdCheck}
+                onKeyPress={this.handleEnterKey}
                 error={!isRepwdValid}
               />
             </div>
             <Button
-              disabled={!(isEmailValid & isPwdValid & isRepwdValid)}
+              disabled={
+                this.state.email === "" ||
+                this.state.password === "" ||
+                this.state.repassword === "" ||
+                isEmailValid === false ||
+                isPwdValid === false ||
+                isRepwdValid === false
+              }
               onClick={this.handleProfile}
             >
               Next
@@ -159,6 +169,7 @@ class SignUpPage extends Component {
                 id="standard-basic"
                 label="name not yet"
                 type="text"
+                onChange={this.handleNameCheck}
                 error={!isNameValid}
               />
             </div>
@@ -171,8 +182,16 @@ class SignUpPage extends Component {
                 error={!isStatusValid}
               />
             </div>
-            <Button text="choose image..." />
-            <Button disabled={!(isNameValid & isStatusValid)}>Next</Button>
+
+            <ImageUploader //TODO Need to modify Upload component design
+              withPreview={true}
+              withLabel={false}
+              buttonText="Choose image"
+            />
+            <Button disabled={this.state.name === "" || isNameValid === false}>
+              Sign Up
+            </Button>
+
             {/* example code - undesigned */}
           </div>
         </Slide>
@@ -180,10 +199,9 @@ class SignUpPage extends Component {
     );
   }
 
-  
-  TODO //should solve asynchronous problem. => e.target.value doesn't get immediately
+  //TODO Should solve asynchronous problem. => e.target.value doesn't get immediately
   // handleType = async (e) => {
-  //   
+  //
   //   const value = await e.target.value;
   //   alert(value);
   //   this.setState(
@@ -220,39 +238,71 @@ class SignUpPage extends Component {
     });
   };
 
+  handleEnterKey = e => {
+    //TODO Need to add profile enter key case condition (after adding signup function)
+    if (
+      (e.charCode === 13) &
+      (this.state.profileOpen === false) &
+      (this.state.isEmailValid &
+        this.state.isPwdValid &
+        this.state.isRepwdValid)
+    ) {
+      this.handleProfile();
+    }
+  };
+
   handleEmailCheck = e => {
     const regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
-    this.setState({ email: e.target.value });
-    if (regExp.test(e.target.value)) {
-      this.setState({ isEmailValid: true });
-    } else {
-      this.setState({ isEmailValid: false });
-    }
+    this.setState({ email: e.target.value }, () => {
+      if (this.state.email === "") {
+        this.setState({ isEmailValid: true });
+      } else if (regExp.test(this.state.email)) {
+        this.setState({ isEmailValid: true });
+      } else {
+        this.setState({ isEmailValid: false });
+      }
+    });
   };
 
   handlePwdCheck = e => {
     const regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/;
-    this.setState({ password: e.target.value });
-    if (regExp.test(e.target.value)) {
-      this.setState({ isPwdValid: true });
-    } else {
-      this.setState({ isPwdValid: false });
-    }
+    this.setState({ password: e.target.value }, () => {
+      if (regExp.test(this.state.password)) {
+        this.setState({ isPwdValid: true });
+      } else {
+        this.setState({ isPwdValid: false });
+      }
+      if (this.state.password !== this.state.repassword) {
+        this.setState({ isRepwdValid: false });
+      } else if (this.state.password === this.state.repassword) {
+        this.setState({ isRepwdValid: true });
+      }
+    });
   };
 
   handleRepwdCheck = e => {
+    if (e.keyCode === 13) {
+      alert("Enter!");
+    }
+    const regExp = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/;
     this.setState({ repassword: e.target.value }, () => {
-      if (this.state.password === this.state.repassword) {
+      if (
+        regExp.test(this.state.repassword) &
+        (this.state.password !== "") &
+        (this.state.password === this.state.repassword)
+      ) {
         this.setState({ isRepwdValid: true });
-      } else {
+      } else if (this.state.password !== this.state.repassword) {
         this.setState({ isRepwdValid: false });
       }
     });
   };
 
   handleNameCheck = e => {
-    // server side check needs to be done
-    this.setState({ name: e.target.value });
+    //TODO Server side check needs to be done
+    this.setState({ name: e.target.value }, () => {
+      console.log(this.state.name);
+    });
   };
 
   handleStatusCheck = e => {
