bc2200ba21ee3fd4f6b66f7939bba2f54296923c
chlwodud77 <uniform3000@naver.com>
2020-02-05 17:07:19 +0900

    [save] Add post component layout

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -6,11 +6,33 @@ const propTypes = {};
 class Post extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = { contentHeight: ["300px", "200px"] };
   }
 
   render() {
-    return <div className="post">Post</div>;
+    const { contentHeight } = this.state;
+    return (
+      <div className="post">
+        <div className="post__header">
+          <div className="post__header-profile">PIC</div>
+          <div className="post__header-name">Jay</div>
+          <div className="post__header-option">OPT</div>
+        </div>
+        <div className="post__content" style={{ height: contentHeight[0] }}>
+          IMAGE
+        </div>
+        <div className="post__desc">DESCRIPTION - user describe about post</div>
+        <div className="post__share">
+          <div className="post__share-like">LIKE</div>
+          <div className="post__share-comment">COMMENT</div>
+        </div>
+        <div className="post__comments">
+          <div className = "post__comments-comment">
+            <span>Jay</span><span> / test comment. </span>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
