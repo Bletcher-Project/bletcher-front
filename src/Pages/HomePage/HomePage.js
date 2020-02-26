9cc9bf061da3e3be50b31703aca343c868bcbfc9
Seogeurim <geulims@naver.com>
2020-02-26 11:27:15 +0900

    PostAction create

--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
+
 import { connect } from "react-redux";
+import * as PostAction from "../../Redux/Actions/PostAction";
 
 import { NavBar, Post } from "../../Components";
 
@@ -10,13 +12,15 @@ const mapStateToProps = state => {
   return {
     isLogin: state.authReducer.isLogin,
     token: state.authReducer.token,
-    user: state.authReducer.user
+    user: state.authReducer.user,
+    feed: state.postReducer.feed
   };
 };
 
 class HomePage extends Component {
   constructor(props) {
     super(props);
+    this.state = { feed: null, feedLoading: true };
     this.state = {
       posts: [
         {
@@ -102,11 +106,27 @@ class HomePage extends Component {
     };
   }
 
+  componentWillMount() {
+    this.getAllPosts();
+  }
+
   render() {
     const posts = this.state.posts;
     return (
       <div className="homePage">
         <NavBar />
+        <div className="homePage__feed">
+          {
+            !this.state.feedLoading
+              ? this.state.feed.map((data, index) => {
+                return (
+                  <Post className="homePage__post" />
+                );
+              })
+              : null
+          }
+        </div>
+        {/* <Post className="homePage__post" /> */}
         {posts.map(post => (
           <Post
             className="homePage__post mb-2 mt-2"
@@ -124,6 +144,12 @@ class HomePage extends Component {
         ))}
       </div>
     );
+  }
+
+  getAllPosts = () => {
+    this.props.dispatch(PostAction.getAllPosts()).then(result => {
+      this.setState({ feed: result, feedLoading: false });
+    });
   }
 }
 
