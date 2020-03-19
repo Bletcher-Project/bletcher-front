72655ff8fb90d05d83d8897d4609894dc4c3e1b8
Seogeurim <geulims@naver.com>
2020-03-19 18:05:13 +0900

    add myPage header (user profile)

--- src/Pages/UserPage/UserPage.js
+++ src/Pages/UserPage/UserPage.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
 
+import { connect } from "react-redux";
+import * as AuthAction from "../../Redux/Actions/AuthAction";
+
 import { NavBar, Thumbnail } from "../../Components";
 
 const defaultProps = {};
 const propTypes = {};
+
+const mapStateToProps = state => {
+  return {
+    user: state.authReducer.user
+  };
+};
 
 class UserPage extends Component {
   constructor(props) {
@@ -12,12 +21,37 @@ class UserPage extends Component {
   }
 
   render() {
+    const { user } = this.props;
+    console.log(user);
     return (
       <div className="userPage">
         <NavBar isActive="user" />
-        <div className="mt-5 pt-5 ml-5">
-          <Thumbnail size="150" src={null} />
-        </div>
+        {user ? (
+          <div className="userPage__contents">
+            <div className="userPage__contents__header">
+              <Thumbnail size="150" src={null} type={user.type} />
+              <div className="userPage__contents__header__profile">
+                <div className="userPage__contents__header__profile-name">
+                  <h1>{user.name}</h1>
+                  <div id="nameUnderBar"></div>
+                </div>
+                <div className="userPage__contents__header__profile-status">
+                  <p>{user.status}</p>
+                </div>
+                <div className="userPage__contents__header__profile-follow">
+                  <div className="followBox">
+                    <span className="followTitle">Followers</span><span id="followerNum">0</span>
+                  </div>
+                  <div className="followBox">
+                    <span className="followTitle">Following</span><span id="followingNum">0</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : null}
+
+
       </div>
     );
   }
@@ -26,4 +60,4 @@ class UserPage extends Component {
 UserPage.defaultProps = defaultProps;
 UserPage.propTypes = propTypes;
 
-export default UserPage;
+export default connect(mapStateToProps)(UserPage);
