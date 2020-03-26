42deb014a0c16913d24c65f441eb5b3360349776
chlwodud77 <uniform3000@naver.com>
2020-03-26 17:31:29 +0900

    [save] Add post upload view (prototype #2) (not designed)
    - Take out modal view and apply just simple appearance transition
    - Transfer user profileImgName, usertype props to upload view
    - Add back button to get homepage feed again

--- src/Components/Upload/Upload.js
+++ src/Components/Upload/Upload.js
@@ -1,105 +1,47 @@
 import React from "react";
 
-import { makeStyles } from "@material-ui/core/styles";
-import Modal from "@material-ui/core/Modal";
-import Backdrop from "@material-ui/core/Backdrop";
-import Zoom from "@material-ui/core/Zoom";
-import Avatar from "@material-ui/core/Avatar";
-import Card from "@material-ui/core/Card";
-import CardHeader from "@material-ui/core/CardHeader";
+import { ServerEndPoint } from "../../Configs/Server";
+
+import { Thumbnail } from "../../Components";
+
 import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
 import Button from "@material-ui/core/Button";
 import TextField from "@material-ui/core/TextField";
 
-import uploadButton from "../../Assets/images/upload-pencil.svg";
-
-const useStyles = makeStyles(theme => ({
-  root: {
-    minWidth: 450,
-    minHeight: 300,
-    "& :focus": {
-      outline: "none"
-    }
-  },
-  title: {
-    fontSize: 14
-  },
-  pos: {
-    marginBottom: 12
-  },
-  field: {
-    width: "100%"
-  },
-  modal: {
-    "& .MuiPaper-root:focus": {
-      outline: "none"
-    },
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center"
-  },
-  center: {
-    justifyContent: "center"
-  }
-}));
+import backIcon from "../../Assets/icons/signup_back.svg";
 
 export default function Upload(props) {
-  const classes = useStyles();
-  const [open, setOpen] = React.useState(false);
-
-  const handleOpen = () => {
-    setOpen(true);
-  };
-
-  const handleClose = () => {
-    setOpen(false);
-  };
-
   return (
-    <div style={props.style}>
+    <div>
       <img
-        onClick={handleOpen}
-        width="50px"
-        src={uploadButton}
-        alt="pencil-post"
+        alt="back"
+        src={backIcon}
+        width="35px"
         style={{ cursor: "pointer" }}
+        onClick={props.handlePrevStep}
       />
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        className={classes.modal}
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500
-        }}
-      >
-        <Zoom in={open} timeout={350}>
-          <Card className={classes.root}>
-            <CardHeader
-              avatar={<Avatar />}
-              title={props.user ? props.user : "Bletcher"}
-            />
-            <CardContent>
-              <TextField
-                className={classes.field}
-                id="outlined-multiline"
-                placeholder="Type your art..."
-                multiline
-                rows="8"
-                rowsMax="10"
-                variant="outlined"
-              />
-            </CardContent>
-            <CardActions className={classes.center}>
-              <Button size="small">Upload</Button>
-            </CardActions>
-          </Card>
-        </Zoom>
-      </Modal>
+      <Thumbnail
+        size="50"
+        src={
+          props.userProfileImg !== null
+            ? `${ServerEndPoint}image/profile/${props.userProfileImg}`
+            : null
+        }
+        type={props.userType}
+      />
+      <div className="postUpload__content">
+        <TextField
+          id="outlined-multiline"
+          placeholder="Type your art..."
+          multiline
+          rows="8"
+          rowsMax="10"
+          variant="outlined"
+        />
+      </div>
+      <CardActions>
+        <Button size="small">Upload</Button>
+      </CardActions>
     </div>
   );
 }
