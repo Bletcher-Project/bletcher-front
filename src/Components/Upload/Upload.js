88b2de3d91a0760bbf501eaa47137c7ef2c11a55
chlwodud77 <uniform3000@naver.com>
2020-03-29 23:17:44 +0900

    Add Creator upload view layout (prototype#1)

--- src/Components/Upload/Upload.js
+++ src/Components/Upload/Upload.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 
 import { ServerEndPoint } from "../../Configs/Server";
 
@@ -10,38 +10,78 @@ import TextField from "@material-ui/core/TextField";
 
 import backIcon from "../../Assets/icons/signup_back.svg";
 
-export default function Upload(props) {
-  return (
-    <div>
-      <img
-        alt="back"
-        src={backIcon}
-        width="35px"
-        style={{ cursor: "pointer" }}
-        onClick={props.handlePrevStep}
-      />
-      <Thumbnail
-        size="50"
-        src={
-          props.userProfileImg !== null
-            ? `${ServerEndPoint}image/profile/${props.userProfileImg}`
-            : null
-        }
-        type={props.userType}
-      />
-      <div className="postUpload__content">
-        <TextField
-          id="outlined-multiline"
-          placeholder="Type your art..."
-          multiline
-          rows="8"
-          rowsMax="10"
-          variant="outlined"
+class Upload extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pictureImg: null,
+      pictureImgUrl: null
+    };
+  }
+
+  render() {
+    return (
+      <div className="postUpload">
+        <img
+          alt="back"
+          src={backIcon}
+          width="35px"
+          style={{ cursor: "pointer" }}
+          onClick={this.props.handlePrevStep}
         />
+
+        {parseInt(this.props.userType) === 1 ? (
+          <div className="postUpload__creator">
+            <input
+              accept="image/*"
+              type="file"
+              name="img"
+              id="art-upload"
+              style={{ display: "none" }}
+              onChange={this.handlePictureImg}
+            />
+            <div className="postUpload__creator-picture">
+              <label htmlFor="art-upload">
+                <img
+                  src={
+                    this.state.pictureImgUrl
+                      ? this.state.pictureImgUrl
+                      : "https://cdn1.iconfinder.com/data/icons/hawcons/32/699085-icon-93-inbox-upload-512.png"
+                  }
+                  width="100%"
+                />
+              </label>
+            </div>
+            <TextField
+              id="outlined-multiline"
+              placeholder="Type your art..."
+              multiline
+              rows="3"
+              rowsMax="10"
+              variant="outlined"
+              fullWidth
+            />
+          </div>
+        ) : (
+          <div className="postUpload__sketcher">It's for Sketcher</div>
+        )}
+
+        <div className="postUpload__upload">
+          <Button size="small">Upload</Button>
+        </div>
       </div>
-      <CardActions>
-        <Button size="small">Upload</Button>
-      </CardActions>
-    </div>
-  );
+    );
+  }
+
+  handlePictureImg = e => {
+    this.setState({ pictureImg: e.target.files[0] }, () => {
+      return this.state.pictureImg
+        ? this.setState({
+            pictureImgUrl: URL.createObjectURL(this.state.pictureImg)
+          })
+        : null;
+    });
+  };
 }
+
+export default Upload;
--- src/Pages/HomePage/HomePage.js
+++ src/Pages/HomePage/HomePage.js
@@ -35,7 +35,7 @@ class HomePage extends Component {
         <NavBar isActive="feed" />
 
         {newPostClicked ? (
-          <div className="homePage__newPost">
+          <div className="homePage__postInput">
             <Upload
               userProfileImg={this.props.user.profileImgName}
               userType={this.props.user.type}
@@ -43,35 +43,37 @@ class HomePage extends Component {
             />
           </div>
         ) : (
-          <div className="homePage__postList">
-            <div onClick={this.toggleNewPost}>What are you thinking now?</div>
-            {!this.state.feedLoading && this.props.user && this.state.feed
-              ? this.state.feed.map(data => {
-                  return (
-                    <Post
-                      key={data.id}
-                      postId={data.id}
-                      className="homePage__post"
-                      isMyPost={this.props.user.id === data.UserId}
-                      userProfileImg={data.User.profileImgName}
-                      userName={data.User.name}
-                      userType={data.User.type}
-                      postContent={data.content}
-                      postHashTags={[
-                        { id: 1, tags: "flower" },
-                        { id: 2, tags: "sunny" }
-                      ]} //////
-                      postImg={data.postImgName}
-                      postDate={data.createdAt}
-                      postLike={135440} //////
-                      postComments={[
-                        { id: 1, author: "Endrew", comment: "good job" },
-                        { id: 2, author: "Sdi_dk", comment: "awesome" }
-                      ]} //////
-                    />
-                  );
-                })
-              : null}
+          <div>
+            <div className="homePage__newPost" onClick={this.toggleNewPost}>What are you thinking now?</div>
+            <div className="homePage__postList">
+              {!this.state.feedLoading && this.props.user && this.state.feed
+                ? this.state.feed.map(data => {
+                    return (
+                      <Post
+                        key={data.id}
+                        postId={data.id}
+                        className="homePage__post"
+                        isMyPost={this.props.user.id === data.UserId}
+                        userProfileImg={data.User.profileImgName}
+                        userName={data.User.name}
+                        userType={data.User.type}
+                        postContent={data.content}
+                        postHashTags={[
+                          { id: 1, tags: "flower" },
+                          { id: 2, tags: "sunny" }
+                        ]} //////
+                        postImg={data.postImgName}
+                        postDate={data.createdAt}
+                        postLike={135440} //////
+                        postComments={[
+                          { id: 1, author: "Endrew", comment: "good job" },
+                          { id: 2, author: "Sdi_dk", comment: "awesome" }
+                        ]} //////
+                      />
+                    );
+                  })
+                : null}
+            </div>
           </div>
         )}
       </div>
