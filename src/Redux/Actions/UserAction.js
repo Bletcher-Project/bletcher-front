00cb68c89fed824a91d2c4edc2cf759a97a40db3
Seogeurim <geulims@naver.com>
2020-04-04 02:08:19 +0900

    redux code clean up

--- src/Redux/Actions/UserAction.js
+++ src/Redux/Actions/UserAction.js
@@ -4,22 +4,9 @@ import axios from "axios";
 import {
   SUCCEED_TO_SIGNUP,
   FAILED_TO_SIGNUP,
-  USERTYPE_UPDATED,
-  SIGNUPSTEP_UPDATED,
-  SIGNUP_INFO_UPDATED
+  SUCCEED_TO_GET_USER_BY_USERNAME,
+  FAILED_TO_GET_USER_BY_USERNAME
 } from "../Constants/action-types";
-
-export function updateUserType(payload) {
-  return { type: USERTYPE_UPDATED, payload };
-}
-
-export function updateSignupStep(payload) {
-  return { type: SIGNUPSTEP_UPDATED, payload };
-}
-
-export function updateSignupInfo(payload) {
-  return { type: SIGNUP_INFO_UPDATED, payload };
-}
 
 export function postSignup(payload) {
   axios
@@ -33,4 +20,33 @@ export function postSignup(payload) {
     });
 
   return { type: SUCCEED_TO_SIGNUP };
+}
+
+export const getUserByUserName = username => {
+  return async dispatch => {
+    try {
+      let response = await fetch(ServerEndPoint + `api/users?name=${username}`, {
+        method: "GET",
+      });
+      if (response.status === 200) {
+        let result = await response.json();
+        await dispatch({
+          type: SUCCEED_TO_GET_USER_BY_USERNAME,
+          payload: result.userInfo
+        });
+        return result.userInfo;
+      } else {
+        await dispatch({
+          type: FAILED_TO_GET_USER_BY_USERNAME,
+          payload: null
+        });
+        return "failed";
+      }
+    } catch (error) {
+      dispatch({
+        type: FAILED_TO_GET_USER_BY_USERNAME,
+        payload: { data: "NETWORK_ERROR" }
+      });
+    }
+  }
 }
--- src/Redux/Reducers/UserReducer.js
+++ src/Redux/Reducers/UserReducer.js
@@ -1,42 +1,9 @@
 import {
-  SUCCEED_TO_SIGNUP,
-  USERTYPE_UPDATED,
-  SIGNUPSTEP_UPDATED,
-  SIGNUP_INFO_UPDATED
 } from "../Constants/action-types";
 
-const initialState = {
-  signUpStep: "typePage",
-  userType: "",
-  email: "",
-  name: "",
-  password: "",
-  status: ""
-};
+const initialState = {};
 
 export default function UserReducer(state = initialState, action) {
-  if (action.type === SUCCEED_TO_SIGNUP) {
-    return Object.assign({}, state, {
-      isSignIn: true
-    });
-  }
-  if (action.type === USERTYPE_UPDATED) {
-    return Object.assign({}, state, {
-      userType: action.payload
-    });
-  }
-  if (action.type === SIGNUPSTEP_UPDATED) {
-    return Object.assign({}, state, {
-      signUpStep: action.payload
-    });
-  }
-  if (action.type === SIGNUP_INFO_UPDATED) {
-    return Object.assign({}, state, {
-      email: action.payload.email,
-      name: action.payload.name,
-      password: action.payload.password,
-      status: action.payload.status
-    });
-  }
+  //
   return state;
 }
