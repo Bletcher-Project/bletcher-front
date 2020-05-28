f0c9dbcfc0187984bbd26bb2dbcf61e6d63400e3
chlwodud77 <uniform3000@naver.com>
2020-05-28 19:32:03 +0900

    Add Sketcher upload function

--- src/Components/Upload/Upload.js
+++ src/Components/Upload/Upload.js
@@ -13,17 +13,19 @@ import backIcon from "../../Assets/icons/signup_back.svg";
 const defaultProps = {};
 const propTypes = {};
 
-const mapStateToProps = state => {
+const mapStateToProps = (state) => {
   return {
     token: state.authReducer.token,
-    user: state.authReducer.user
+    user: state.authReducer.user,
   };
 };
 
-const mapDispatchToProps = dispatch => {
+const mapDispatchToProps = (dispatch) => {
   return {
     uploadPost: (params, token) =>
-      dispatch(PostAction.uploadPost(params, token))
+      dispatch(PostAction.uploadPost(params, token)),
+    uploadSketcherPost: (params, token) =>
+      dispatch(PostAction.uploadSketcherPost(params, token)),
   };
 };
 
@@ -34,7 +36,11 @@ class Upload extends Component {
       pictureImg: null,
       pictureImgUrl: null,
       croppedImgUrl: null,
-      content: ""
+      contentImg: null,
+      contentImgUrl: null,
+      styleImg: null,
+      styleImgUrl: null,
+      content: "",
     };
   }
 
@@ -76,7 +82,7 @@ class Upload extends Component {
                   className="cropper"
                   alt="original"
                   src={this.state.pictureImgUrl}
-                  ref={cropper => {
+                  ref={(cropper) => {
                     this.cropper = cropper;
                   }}
                   // Cropper.js options
@@ -104,34 +110,40 @@ class Upload extends Component {
             <input
               accept="image/*"
               type="file"
-              name="img"
-              id="art-upload"
+              name="content"
+              id="content-upload"
               style={{ display: "none" }}
-              onChange={this.handlePictureImg}
+              onChange={this.uploadContentImg}
+            />
+            <input
+              accept="image/*"
+              type="file"
+              name="style"
+              id="style-upload"
+              style={{ display: "none" }}
+              onChange={this.uploadStyleImg}
             />
             <div className="postUpload__sketcher-uploadPic">
               <div>
                 <Button>
-                  <label htmlFor="art-upload">upload image</label>
+                  <label htmlFor="content-upload">upload image</label>
                 </Button>
-                {this.state.pictureImgUrl === null ? null : (
+                {this.state.contentImgUrl === null ? null : (
                   <img
-                    alt="original"
+                    alt="content"
                     width="100%"
-                    src={this.state.pictureImgUrl}
+                    src={this.state.contentImgUrl}
                   />
                 )}
               </div>
               <div>
                 <Button>
-                  <label htmlFor="art-upload">select style image</label>
+                  <label htmlFor="style-upload">select style image</label>
                 </Button>
                 {/* It is sample picture for later */}
-                <img
-                  alt="style_preview"
-                  width="100%"
-                  src="https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/71Hk/image/Kdo1cF-dckIZlXOCIIKene-sSmo.jpg"
-                />
+                {this.state.styleImgUrl === null ? null : (
+                  <img alt="style" width="100%" src={this.state.styleImgUrl} />
+                )}
               </div>
             </div>
 
@@ -160,17 +172,49 @@ class Upload extends Component {
     );
   }
 
-  handlePictureImg = e => {
+  uploadContentImg = (e) => {
+    this.setState(
+      {
+        contentImg:
+          e.target.files[0] !== undefined
+            ? e.target.files[0]
+            : this.state.contentImg,
+      },
+      () => {
+        this.setState({
+          contentImgUrl: URL.createObjectURL(this.state.contentImg),
+        });
+      }
+    );
+  };
+
+  uploadStyleImg = (e) => {
+    this.setState(
+      {
+        styleImg:
+          e.target.files[0] !== undefined
+            ? e.target.files[0]
+            : this.state.styleImg,
+      },
+      () => {
+        this.setState({
+          styleImgUrl: URL.createObjectURL(this.state.styleImg),
+        });
+      }
+    );
+  };
+
+  handlePictureImg = (e) => {
     this.setState(
       {
         pictureImg:
           e.target.files[0] !== undefined
             ? e.target.files[0]
-            : this.state.pictureImg
+            : this.state.pictureImg,
       },
       () => {
         this.setState({
-          pictureImgUrl: URL.createObjectURL(this.state.pictureImg)
+          pictureImgUrl: URL.createObjectURL(this.state.pictureImg),
         });
       }
     );
@@ -179,32 +223,64 @@ class Upload extends Component {
   /*
   Post Upload functions
 */
-  handleContent = e => {
+  handleContent = (e) => {
     this.setState({ content: e.target.value });
   };
 
   handlePostUpload = () => {
-    if (this.state.pictureImg) {
-      const imgInfo = this.cropper.getCropBoxData();
-      this.cropper
-        .getCroppedCanvas({ imageSmoothingQuality: "high" })
-        .toBlob(async croppedImg => {
+    switch (parseInt(this.props.userType)) {
+      case 1:
+        if (this.state.pictureImgUrl) {
+          const imgInfo = this.cropper.getCropBoxData();
+          this.cropper
+            .getCroppedCanvas({ imageSmoothingQuality: "high" })
+            .toBlob(async (croppedImg) => {
+              const params = new FormData();
+              params.append("img", croppedImg, this.state.pictureImg.name);
+              params.append("content", this.state.content);
+              params.append("UserId", this.props.userId);
+              params.append("width", imgInfo.width);
+              params.append("height", imgInfo.height);
+              const postUpload = await this.props.uploadPost(
+                params,
+                this.props.token
+              );
+              return postUpload
+                ? window.location.reload()
+                : alert("upload failed!");
+            });
+        } else {
+          alert("Please upload your art first :)");
+        }
+        break;
+      case 0:
+        if (this.state.contentImgUrl && this.state.styleImgUrl) {
           const params = new FormData();
-          params.append("img", croppedImg, this.state.pictureImg.name);
+          params.append(
+            "img",
+            this.state.contentImg,
+            this.state.contentImg.name
+          );
+          params.append("img", this.state.styleImg, this.state.styleImg.name);
           params.append("content", this.state.content);
           params.append("UserId", this.props.userId);
-          params.append("width", imgInfo.width);
-          params.append("height", imgInfo.height);
-          const postUpload = await this.props.uploadPost(
-            params,
-            this.props.token
-          );
-          return postUpload
-            ? window.location.reload()
-            : alert("upload failed!");
-        });
-    } else {
-      alert("Please upload your art first :)");
+          params.append("width", parseInt(200));
+          params.append("height", parseInt(300));
+          const postSketcherUpload = async () => {
+            const upload = await this.props.uploadSketcherPost(
+              params,
+              this.props.token
+            );
+            return upload ? window.location.reload() : alert("upload failed!");
+          };
+          postSketcherUpload();
+        } else {
+          alert("Please upload your art first :)");
+        }
+        break;
+
+      default:
+        alert("Wrong Access!");
     }
   };
 }
