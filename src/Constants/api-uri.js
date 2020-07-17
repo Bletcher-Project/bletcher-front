9fe5c59a2ec8c8727dda2ea4231ace4c9c9d3dbe
Seogeurim <geulims@naver.com>
2020-07-17 23:55:05 +0900

    modify api-uri constant

---
+++ src/Constants/api-uri.js
@@ -0,0 +1,21 @@
+/* init */
+export const INIT = '/api';
+
+/* auth */
+export const AUTH_API = '/auth';
+
+/* users */
+export const USER_API = '/users';
+
+/* posts */
+export const POST_API = '/posts';
+export const ONE = '/one';
+export const LIKE = '/like';
+
+/* query */
+export const QUERY_EMAIL = '?email=';
+export const QUERY_NAME = '?name=';
+
+/* temporary */
+export const SKETCHER = '/sketcher';
+export const SIGNIN = '/signin';
