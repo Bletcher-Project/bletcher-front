8c12d0c6726f77a1364e523b4b3fc4f85ceb34ea
Kwon <khj9709@icloud.com>
2020-09-20 17:08:09 +0900

    add fetch fundingPost method #43

--- src/Redux/fetch-post.js
+++ src/Redux/fetch-post.js
@@ -4,13 +4,18 @@ import { INIT, POST_API } from 'Constants/api-uri';
 
 const initialState = {
   mainPost: null,
+  fundingPost: null,
 };
 
 const GET_MAIN_POSTS_SUCCESS = 'post/GET_MAIN_POSTS_SUCCESS';
 const GET_MAIN_POSTS_FAIL = 'post/GET_MAIN_POSTS_FAIL';
+const GET_FUNDING_POSTS_SUCCESS = 'post/GET_FUNDING_POSTS_SUCCESS';
+const GET_FUNDING_POSTS_FAIL = 'post/GET_FUNDING_POSTS_FAIL';
 
 export const getMainPostsSuccess = createAction(GET_MAIN_POSTS_SUCCESS); // result.posts
 export const getMainPostsFail = createAction(GET_MAIN_POSTS_FAIL);
+export const getFundingPostsSuccess = createAction(GET_FUNDING_POSTS_SUCCESS); // result.posts
+export const getFundingPostsFail = createAction(GET_FUNDING_POSTS_FAIL);
 
 export default fetchPostReducer(
   {
@@ -18,6 +23,12 @@ export default fetchPostReducer(
       return { ...state, mainPost: action.payload };
     },
     [GET_MAIN_POSTS_FAIL]: (state) => {
+      return { ...state };
+    },
+    [GET_FUNDING_POSTS_SUCCESS]: (state, action) => {
+      return { ...state, fundingPost: action.payload };
+    },
+    [GET_FUNDING_POSTS_FAIL]: (state) => {
       return { ...state };
     },
   },
@@ -40,6 +51,28 @@ export const getMainPosts = () => {
       return null;
     } catch (error) {
       await dispatch(getMainPostsFail());
+      return null;
+    }
+  };
+};
+
+export const getFundingPosts = () => {
+  return async (dispatch) => {
+    try {
+      const response = await fetch(
+        `${process.env.REACT_APP_SERVER_URL}${INIT}${POST_API}`,
+        { method: 'GET' },
+      );
+      // TO DO :: funding post get api
+      if (response.statuS === 200) {
+        const result = response.json();
+        await dispatch(getFundingPostsSuccess(result.data));
+        return result.data;
+      }
+      await dispatch(getFundingPostsSuccess());
+      return null;
+    } catch (error) {
+      await dispatch(getFundingPostsFail());
       return null;
     }
   };
