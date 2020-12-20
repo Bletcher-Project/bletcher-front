2d1ef7fc5116b1d4024b1f13a11c815503306ef3
Seogeurim <geulims@naver.com>
2020-12-20 17:18:22 +0900

    Create SignInModal Component #53

---
+++ src/Components/Sign/SignInModal/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const defaultProps = {};
+const propTypes = {};
+
+function SignInModal(props) {
+  const {} = props;
+
+  return <></>;
+}
+
+SignInModal.defaultProps = defaultProps;
+SignInModal.propTypes = propTypes;
+
+export default SignInModal;
