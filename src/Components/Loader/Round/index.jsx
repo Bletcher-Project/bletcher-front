46df557f7bd0e63ac06ecdbe0933fa8ded8d368a
Seogeurim <geulims@naver.com>
2020-12-23 23:50:37 +0900

    add div of RoundLoader #55

--- src/Components/Loader/Round/index.jsx
+++ src/Components/Loader/Round/index.jsx
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function RoundLoader() {
-  return <div className="roundLoader">This is RoundLoader Component</div>;
+  return (
+    <div className="roundLoader">
+      <div className="roundLoader__spinner">
+        <div className="roundLoader__spinner-blade" />
+        <div className="roundLoader__spinner-blade" />
+        <div className="roundLoader__spinner-blade" />
+        <div className="roundLoader__spinner-blade" />
+        <div className="roundLoader__spinner-blade" />
+        <div className="roundLoader__spinner-blade" />
+        <div className="roundLoader__spinner-blade" />
+        <div className="roundLoader__spinner-blade" />
+        <div className="roundLoader__spinner-blade" />
+        <div className="roundLoader__spinner-blade" />
+        <div className="roundLoader__spinner-blade" />
+        <div className="roundLoader__spinner-blade" />
+      </div>
+    </div>
+  );
 }
 
 export default RoundLoader;
