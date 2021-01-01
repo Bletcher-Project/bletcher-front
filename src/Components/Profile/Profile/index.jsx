2ac5abf6079f252801aa0351c759fd300833cf04
Seogeurim <geulims@naver.com>
2021-01-01 23:01:11 +0900

    set profile content layout #57

--- src/Components/Profile/Profile/index.jsx
+++ src/Components/Profile/Profile/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
+import Thumbnail from 'Components/Thumbnail';
 import RoundInput from 'Components/Form/RoundInput';
 
 function Profile() {
   return (
     <div className="profile">
-      <div>image area</div>
+      <Thumbnail />
       <div>
         Bletcher users will be able to identify you with the information below.
       </div>
       <div>
         <RoundInput />
+        <RoundInput />
       </div>
+      <RoundInput />
+      <RoundInput />
+      <RoundInput />
     </div>
   );
 }
