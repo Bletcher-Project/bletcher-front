a4df4c4abb0d89433b1402b5ceb743fd59154684
Kwon <khj9709@icloud.com>
2021-01-10 17:43:47 +0900

    fix mixChecker null return error #48

--- src/Components/Mix/MixChecker/index.jsx
+++ src/Components/Mix/MixChecker/index.jsx
@@ -6,11 +6,13 @@ import ProgressBar from 'Components/Common/ProgressBar';
 function MixChecker() {
   const mixState = useSelector((state) => state.postReducer.mixState);
   const { mixId, isMixing } = mixState;
-  return (
-    (isMixing || mixId) && (
-      <ProgressBar width={50} height={3} barSize={0.8} value={0} />
-    )
-  );
+  const toRtn = () => {
+    if (isMixing || mixId)
+      return <ProgressBar width={50} height={3} barSize={0.8} value={0} />;
+    return <div />;
+  };
+
+  return toRtn();
 }
 
 export default MixChecker;
