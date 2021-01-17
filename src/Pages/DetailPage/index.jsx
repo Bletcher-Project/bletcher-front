b5a59564e6ceccb70c87ca3327c6b67186b5244e
Kwon <khj9709@icloud.com>
2021-01-17 16:59:20 +0900

    modify detail page layout #61

--- src/Pages/DetailPage/index.jsx
+++ src/Pages/DetailPage/index.jsx
@@ -7,14 +7,14 @@ import { connect } from 'react-redux';
 import { getPostByPostId } from 'Redux/post';
 
 import NavBar from 'Components/Common/NavBar';
-import FavoriteButton from 'Components/Post/PostButton/FavoriteButton';
-import MixButton from 'Components/Post/PostButton/MixButton';
 
-import DueDate from 'Assets/icons/DueDate';
-import HeartImg from 'Assets/images/fundHeart-bg-removed.png';
+import DueDate from 'Assets/images/dueDate-filled.svg';
+import HeartImg from 'Assets/images/fundHeart-bg-purple.png';
+
+import profile1 from 'Dummies/dummyImage/1.jpg';
+import profile2 from 'Dummies/dummyImage/2.jpg';
 
 import queryString from 'query-string';
-import cx from 'classnames';
 
 const defaultProps = {};
 const propTypes = {
@@ -38,103 +38,83 @@ class DetailPage extends Component {
   getSrc = () => {
     const { post } = this.state;
     if (post.Image !== undefined) return post.Image.path;
-    return post['Image.path'];
+    return post && post['Image.path'];
   };
 
   getPostIdByQuery = () => {
     const { location } = this.props;
     const query = queryString.parse(location.search);
     const { postId } = query;
+
     return postId;
   };
 
   componentDidMount = async () => {
     const { getPost } = this.props;
     const postId = this.getPostIdByQuery();
-
     if (postId) {
       const detailedPost = await getPost(postId);
       await new Promise((accept) =>
         this.setState({ post: detailedPost }, accept),
       );
+    } else {
+      window.location.replace('/notFound');
     }
   };
 
   render() {
-    const active = 'funding';
     return (
-      <>
+      <div className="wrapper">
         <NavBar isActive="detail" />
         <div className="detailPage">
           <div className="expand" />
           <div className="detailPage__content">
-            <div className="detailPage__content__imageBox">
-              <div className="detailPage__content__imageBox__inner">
-                <img
-                  className="detailPage__content__imageBox__image"
-                  src={this.getSrc()}
-                  alt="postImage"
-                />
-              </div>
+            <div className="detailPage__content__inner">
+              <img
+                className="detailPage__content__image"
+                src={this.getSrc()}
+                alt="postImage"
+              />
             </div>
           </div>
 
-          <div
-            className={cx('detailPage__rightTab', {
-              fundInfo: active === 'funding',
-            })}
-          >
-            {active === 'funding' ? (
-              <>
-                <div className="detailPage__rightTab fundInfo__buttons">
-                  <div>
-                    <span>
-                      <DueDate fill />
-                    </span>
-                    <span>1:32:21</span>
-                  </div>
-                  <div>
-                    <span>
-                      <img src={HeartImg} alt="fund-heart" />
-                    </span>
-                    <span>192</span>
-                  </div>
-                </div>
-                <div className="detailPage__rightTab fundInfo__authors">
-                  <div className="detailPage__rightTab fundInfo__authors__author">
-                    <span className="detailPage__rightTab fundInfo__authors__author__imgUp">
-                      <img src={null} alt="profile1" />
-                    </span>
-                    <span className="detailPage__rightTab fundInfo__authors__author__name">
-                      HyoJI
-                    </span>
-                  </div>
-                  <div className="authorMargin" />
-                  <div className="detailPage__rightTab fundInfo__authors__author">
-                    <span className="detailPage__rightTab fundInfo__authors__author__name">
-                      hangsoo
-                    </span>
-                    <span className="detailPage__rightTab fundInfo__authors__author__imgDown">
-                      <img src={null} alt="profile2" />
-                    </span>
-                  </div>
-                </div>
-              </>
-            ) : (
-              <div className="detailPage__rightTab__buttons">
-                <div>
-                  <FavoriteButton liked />
-                  <span className="">FAVORITE</span>
-                </div>
-                <div>
-                  <MixButton />
-                  <span className="">BLEND</span>
-                </div>
+          <div className="detailPage__rightTab">
+            <div className="detailPage__rightTab__buttons">
+              <div>
+                <span>
+                  <img src={DueDate} alt="dueDate" />
+                </span>
+                <span>1:32:21</span>
               </div>
-            )}
+              <div>
+                <span>
+                  <img src={HeartImg} alt="fund-heart" />
+                </span>
+                <span>362</span>
+              </div>
+            </div>
+            <div className="detailPage__rightTab__authors">
+              <div className="detailPage__rightTab__authors__author">
+                <span className="detailPage__rightTab__authors__author__img">
+                  <img src={profile1} alt="profile1" />
+                </span>
+                <span className="detailPage__rightTab__authors__author__name">
+                  HyoJI
+                </span>
+              </div>
+              <div className="authorMargin" />
+              <div className="detailPage__rightTab__authors__author">
+                <span className="detailPage__rightTab__authors__author__name">
+                  hangsoo
+                </span>
+                <span className="detailPage__rightTab__authors__author__img">
+                  <img src={profile2} alt="profile2" />
+                </span>
+              </div>
+            </div>
           </div>
         </div>
-      </>
+      </div>
     );
   }
 }
