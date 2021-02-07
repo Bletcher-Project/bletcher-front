1b091fb264522d73e2a601a4064fae87ae367e59
Kwon <khj9709@icloud.com>
2021-02-07 23:08:02 +0900

    remove redux-persist #48

--- src/index.jsx
+++ src/index.jsx
@@ -3,9 +3,6 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-import { PersistGate } from 'redux-persist/integration/react';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './Redux';
@@ -15,27 +12,16 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.scss';
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const enhancedReducer = persistReducer(persistConfig, reducers);
-
 const store = createStore(
-  enhancedReducer,
+  reducers,
   composeWithDevTools(applyMiddleware(ReduxThunk)),
 );
 
-const persistor = persistStore(store);
-
 ReactDOM.render(
   <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </PersistGate>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </Provider>,
   document.getElementById('root'),
 );
