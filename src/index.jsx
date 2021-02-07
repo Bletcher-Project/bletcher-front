c54d28a29377ca34db53d644ae634439c5612f8e
Kwon <khj9709@icloud.com>
2021-02-07 18:24:17 +0900

    install & apply redux-persist #48

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import ReduxThunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import { PersistGate } from 'redux-persist/integration/react';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reducers from './Redux';
@@ -12,16 +15,27 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.scss';
 
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const enhancedReducer = persistReducer(persistConfig, reducers);
+
 const store = createStore(
-  reducers,
+  enhancedReducer,
   composeWithDevTools(applyMiddleware(ReduxThunk)),
 );
 
+const persistor = persistStore(store);
+
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <PersistGate loading={null} persistor={persistor}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </PersistGate>
   </Provider>,
   document.getElementById('root'),
 );
