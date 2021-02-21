92b8760229231d79c883dc93230f823c8cd17f53
jjinny <khj9709@icloud.com>
2021-02-21 16:47:50 +0900

    Update src/Constants/progressbar-text.js
    Co-authored-by: Seogeurim <geulims@naver.com>

--- src/Constants/progressbar-text.js
+++ src/Constants/progressbar-text.js
@@ -4,7 +4,7 @@ export const pgBarErrorText = 'Error! Try to mix again.';
 export const pgBarText = [
   "Extracting origin's feature...",
   "Extracting sub's feature...",
-  "Checking Bletcher's pallette...",
+  "Checking Bletcher's palette...",
   'Asking Robot to Synthesize...',
   'Mixing...',
   'Almost Done...',
